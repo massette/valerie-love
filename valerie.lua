c8c0d1da4d58cfebc1a21c8dbff834746efe35e0
massette <featherboxen@gmail.com>
2021-12-09 00:50:39 -0500

    Add semantic versioning

--- valerie.lua
+++ valerie.lua
@@ -1,4 +1,28 @@
-local Valerie = {}
+local Valerie = {
+    _VERSION = "Valerie v.1.0.0",
+    _DESCRIPTION = "A thing.",
+    _URL = "https://github.com/massette/",
+
+	_LICENSE = [[Copyright (c) 2021 massette
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.]]
+}
 
 function Valerie:new(o, v, get_value, panel_props)
     o.transitions = o.transitions or {}
