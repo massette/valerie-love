11136794b25c09fe9f349a33992d5548b98349fa
massette <featherboxen@gmail.com>
2021-12-08 22:05:54 -0500

    Add valerie-no-props.lua

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 **Valerie** is a tool to easily transition between values and do weird function stuff. It might be useless; I can't tell.
 
 ## What it Does
-Started this because smooth transitions have always gummed up my code and I wanted a simple way to pass percentages of non-constant values to stuff. It's mainly for use in a GUI thing that I'm working on for Löve2D, but its written entirely in lua, and the `panel_props` argument doesn't break anything if you just ignore it.
+Started this because smooth transitions have always gummed up my code and I wanted a simple way to pass percentages of non-constant values to stuff. It's mainly for use in a GUI thing that I'm working on, but its written entirely in lua, so I guess you could use it for something else if you really wanted to.There's [a version without the weird props stuff](https://github.com/massette/valerie-love/blob/master/valerie-no-props.lua) if you just want the transitions part of it.
---
+++ valerie-no-props.lua
@@ -0,0 +1,119 @@
+local Valerie = {}
+
+function Valerie:new(o, v, get_value)
+    o.transitions = {}
+    o.get = get_value
+    
+    setmetatable(o, {
+        __index = self,
+        __call = Valerie.set
+    })
+
+    o:set(v)
+
+    return o
+end
+
+function Valerie:set(v, ignore_queue)
+    if not (#self.transitions == 0 or ignore_queue) then
+        self.transitions[#self.transitions + 1] = {
+            old = self.real_value,
+            new = v,
+        }
+
+        return self.value
+    end
+
+    self.real_value = v
+    self.value = self:get(v)
+
+    return self.value
+end
+
+function Valerie:get(v)
+    return v
+end
+
+function Valerie:transition(mode, v, delta, ignore_queue)
+    if ignore_queue then
+        self.transitions = {{
+            mode = mode,
+
+            old = self.real_value,
+            new = v,
+            
+            delta = delta or 1
+        }}
+    else
+        self.transitions[#self.transitions + 1] = {
+            mode = mode,
+
+            old = self.real_value,
+            new = v,
+            
+            delta = delta or 1
+        }
+    end
+end
+
+function Valerie:transition_t(mode, v, time, ignore_queue)
+    if ignore_queue then
+        self.transitions = {{
+            mode = mode,
+
+            old = self.real_value,
+            new = v,
+            
+            time = time or 1
+        }}
+    else
+        self.transitions[#self.transitions + 1] = {
+            mode = mode,
+
+            old = self.real_value,
+            new = v,
+            
+            time = time or 1
+        }
+    end
+end
+
+function Valerie:update(dt)
+    if #self.transitions > 0 then
+        local trans = self.transitions[1]
+
+        if not trans.delta and trans.time then
+            trans.delta = math.abs((trans.new - trans.old) / trans.time)
+        end
+
+        if trans.mode == "linear" then
+            if self.real_value < trans.new then
+                local v = self.real_value + (dt * trans.delta)
+                self:set(math.min(v, trans.new), true)
+            elseif self.real_value > trans.new then
+                local v = self.real_value - (dt * trans.delta)
+                self:set(math.max(v, trans.new), true)
+            else
+                self:set(trans.new, true)
+                table.remove(self.transitions, 1)
+
+                if #self.transitions > 0 then
+                    self.transitions[1].old = self.real_value
+                end
+            end
+        else
+            self:set(trans.new, true)
+            table.remove(self.transitions, 1)
+
+            if #self.transitions > 0 then
+                self.transitions[1].old = self.real_value
+            end
+        end
+    end
+end
+
+setmetatable(Valerie, {
+    __call = Valerie.new
+})
+
+return Valerie
